7c72d2c2cbb8b22a1997d4a84a09a3d094dfba09
Sharan107 <75675225+Sharan107@users.noreply.github.com>
2021-05-20 11:16:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,90 +1,42 @@
-var track,carImg,obstacleImg;
-var bgImg,car,obstacle,obstacleGroup;
-var gameState="play";
-var finish=false;
-
+var wheel_img,knife_img,fruit1_img;
+var wheel,knife,fruit1;
 function preload() {
 
-  //preloading images
-
- track=loadImage("images/track.jpg");
- obstacleImg=loadImage("images/stone.png");
- carImg=loadImage("images/racing car.png");
-  
+ 
+  wheel_img=loadImage("images/wheel.png");
+  knife_img=loadImage("images/sword.png");
+  fruit1_img=loadImage("images/apple.png");
 }
 
 function setup() {
-  createCanvas(displayWidth-20,displayHeight-30);
 
-  //creating sprites
-
-  obstacleGroup= new Group();
-
- car=createSprite(displayWidth-20/2,displayHeight-30/2,50,50);
- car.addImage(carImg);
- car.setCollider("rectangle",0,100,250,250);
-
- // console.log(displayHeight)
+  createCanvas(700,700);
+wheel=createSprite(300,300);
+wheel.scale=1.3;
+wheel.addImage(wheel_img);
+//wheel.rotation=270;
+//wheel.debug=false;
+//wheel.setCollider("circle",0,0,105);
+  
+knife=createSprite(300,650);
+knife.addImage(knife_img);
+knife.rotation=-45;
+ 
+fruit1=createSprite(140,300);
+fruit1.addImage(fruit1_img);
+fruit1.scale=0.3;
  }
 
 function draw() {
- background(rgb(198,135,103));
- image(track, 550,-displayHeight*4,displayWidth, displayHeight*5);
-
- //making car move with arrow keys
-if(gameState==="play"){
- if(keyDown(LEFT_ARROW)){
-    car.x=car.x-10;
- }
-if(keyDown(RIGHT_ARROW)){
-  car.x=car.x+10;
-}
-if(keyDown(UP_ARROW)){
-  car.y=car.y-10;
-}
-
-
-if(frameCount%200==0){ 
-  obstacle=createSprite(random(800,1300),camera.position.y-250,30,30);
-  obstacle.addImage(obstacleImg); 
-  obstacleGroup.add(obstacle); 
-} 
-
-//if the car hits the obstacle game is over  
-if(car.isTouching(obstacleGroup)){
-  gameState="end";
+ background(rgb(38,0,73));
+ 
    
-  }
+  //  wheel.rotation=wheel.rotation+10;
 
+    //if(keyDown("up")){
+     // knife.velocityY=-5;
+    //}
+  
 
-  //setting camera directions
-camera.position.x = displayWidth-20/2;
-camera.position.y = car.y;
-
-if(car.y<=-3050){
-   finish=true;
-   gameState="end";
-}
-}
-
-if(gameState==="end"){
- car.destroy(); 
- obstacleGroup.destroyEach(); 
- background("black");
- textSize(40);
- fill("black")
- stroke("white");
- strokeWeight(4);
- 
-if(finish==true){
-  //background("black"); 
- 
-  text("YOU WIN",displayWidth,camera.position.y); 
-}
-else{
-  text("GAMEOVER!!!!",displayWidth-20/2,camera.position.y);
-}
-}
-
-drawSprites();
+ drawSprites();
 }
